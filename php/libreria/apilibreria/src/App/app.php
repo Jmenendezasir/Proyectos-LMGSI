56a6498cb166f08fbb44527c1800623240374c58
Jmenendezasir <jmenendezasir@gmail.com>
2021-02-26 08:58:32 +0100

    test jenkins

--- php/libreria/apilibreria/src/App/app.php
+++ php/libreria/apilibreria/src/App/app.php
@@ -1,17 +1,23 @@
 <?php
-
+ 
 use Slim\Factory\AppFactory;
 use Slim\Exception\NotFoundException;
-
-// cargamos el autoload para que pueda detectar el resto de las clasesdfsdf
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+// cargamos el autoload para que pueda detectar el resto de las clases
 require __DIR__ . '/../../vendor/autoload.php';
-
-
-// creamos la aplicación php sdassdasdsadsaefsdf
+ 
+ 
+// creamos la aplicación php
 $app = AppFactory::create();
-
+$app->setBasePath("/php/libreria/apilibreria/public/index.php");
+$app->get("/hello", function(Request $request, Response $response, $args) {
+    $response->getBody()->write("Buenos días Jenkins");
+    return $response;
+});
+ 
 //Cargamos en memoria los archivos de rutas que contendrán los entrypoints a cada una de las tablas.
 //los entrypoints harán referencia a las acciones CRUD de una tabla de nuestra BD
-require __DIR__ . "/../Routes/Libros.php";
-
+require __DIR__ . "/../Routes/libros.php";
 $app->run();
+ 
