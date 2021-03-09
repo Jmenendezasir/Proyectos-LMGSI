cb32e68276667ef09a46d77670fb4e447e525ec2
Jmenendezasir <jmenendezasir@gmail.com>
2021-03-09 11:53:49 +0100

    hello world

--- php/libreria/apilibreria/src/App/app.php
+++ php/libreria/apilibreria/src/App/app.php
@@ -12,7 +12,7 @@ require __DIR__ . '/../../vendor/autoload.php';
 $app = AppFactory::create();
 $app->setBasePath("/php/libreria/apilibreria/public/index.php");
 $app->get("/hello", function(Request $request, Response $response, $args) {
-    $response->getBody()->write("Buenos días Jenkins");
+    $response->getBody()->write("Hello world");
     return $response;
 });
  
