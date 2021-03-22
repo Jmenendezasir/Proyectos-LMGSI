9767801d03e5cf6b1aa258c85e0df3e2ef569ff2
Jmenendezasir <jmenendezasir@gmail.com>
2021-03-22 18:46:47 +0100

    fallo 2

--- php/libreria/apilibreria/src/Config/config.php
+++ php/libreria/apilibreria/src/Config/config.php
@@ -6,7 +6,7 @@ interface config {
     CONST DB_NAME = "libreria";
     CONST DB_HOST = "localhost";
     CONST DB_PORT = "3306";
-    CONST DB_USER = "root";
+    CONST DB_USER = "jose";
     CONST DB_PASSWORD = "1234";
     CONST DB_CHARSET = "utf8";
 }
