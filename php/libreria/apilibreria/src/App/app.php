ffaa5eb80bb5bacd822272df408c6cf4ad08e689
Jmenendezasir <jmenendezasir@gmail.com>
2021-03-22 18:51:24 +0100

    fallo 3

--- php/libreria/apilibreria/src/App/app.php
+++ php/libreria/apilibreria/src/App/app.php
@@ -11,10 +11,10 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->setBasePath("/php/libreria/apilibreria/public/index.php");
-/*$app->get("/hello", function(Request $request, Response $response, $args) {
+$app->get("/hello", function(Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world");
     return $response;
-}); */
+});
 
 require __DIR__ . "/../Routes/Libros.php";
 require __DIR__ . "/../Routes/Categorias.php";
