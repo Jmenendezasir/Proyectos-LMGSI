d75390a4c108bb0c6f19ac7c607999eff1f6b3db
Jmenendezasir <jmenendezasir@gmail.com>
2021-03-23 11:43:53 +0100

    sdfsdf

--- php/libreria/apilibreria/src/Controllers/joseCDetallePedidosController.php
+++ php/libreria/apilibreria/src/Controllers/joseCDetallePedidosController.php
@@ -12,7 +12,7 @@
             $cantidad = (int)$parametros['cantidad'];
             $descuento = $parametros['descuento'];
             $fecha = $parametros['fecha'];
-            $valores = array($lid, $uid, $cantidad, $descuento, $fecha, $valores);
+            $valores = array($lid, $uid, $cantidad, $descuento, $fecha);
             $result = joseCDetallePedidosModel::joseCnew($valores);
             $resultJson = json_encode($result);
             $response->getBody()->write($resultJson);
