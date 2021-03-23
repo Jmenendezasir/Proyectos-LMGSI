cbce7c63e6b6a8310a36f5e9f69f068e6d9d2336
Jmenendezasir <jmenendezasir@gmail.com>
2021-03-23 10:46:49 +0100

    asdsa

--- php/libreria/apilibreria/src/Routes/joseCUsuarios.php
+++ php/libreria/apilibreria/src/Routes/joseCUsuarios.php
@@ -4,3 +4,4 @@ use Slim\Routing\RouteCollectorProxy;
 
 $app->group('/api', function(RouteCollectorProxy $group){
     $group->post('/joseCUsuarios/new', 'App\Controllers\joseCUsuariosController:joseCnew');
+}
