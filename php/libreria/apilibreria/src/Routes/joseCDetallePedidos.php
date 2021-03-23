9a05dbd143a4ea5834040c45d06604da67303422
Jmenendezasir <jmenendezasir@gmail.com>
2021-03-23 11:41:15 +0100

    asda

--- php/libreria/apilibreria/src/Routes/joseCDetallePedidos.php
+++ php/libreria/apilibreria/src/Routes/joseCDetallePedidos.php
@@ -5,5 +5,5 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 $app->group('/api', function(RouteCollectorProxy $group){
-    $group->post('/pedidos/new', 'App\Controllers\joseCDetallePedidosController:joseCnew');   
+    $group->post('/joseCDetallePedidos/new', 'App\Controllers\joseCDetallePedidosController:joseCnew');   
 });
